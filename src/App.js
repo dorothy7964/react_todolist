75e368c12b8c47e39cd64e8cef4d4499483ca504
SEC\user <dorothy_007@naver.com>
2019-05-15 16:21:57 +0900

    초기 state 정의

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+  id = 3 // 이미 0,1,2 가 존재하므로 3으로 설정
+
+  state = {
+    input : '',
+    todos: [
+      { id: 0, text: ' 리액트 소개1', checked: false },
+      { id: 1, text: ' 리액트 소개2', checked: true },
+      { id: 2, text: ' 리액트 소개3', checked: false }
+    ]
+  }
 
   render() {
+    const { todos } = this.state;
+
     return(
       <TodoListTemplate form={(
           <Form />
+
       )}>
-        <TodoItemList />
+        <TodoItemList
+          todos={todos}
+         />
       </TodoListTemplate>
     );
   }
