1ea929a349fd142fdefc37f87225ec2cbd68dd81
SEC\user <dorothy_007@naver.com>
2019-05-15 17:17:22 +0900

    TodoItem 클래스형으로 수정
    최적화 목적으로 함수형 에서 클래스 컴포넌트로 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TodoItem.css';
 
-const TodoItem = ({ id, text, checked, onToggle, onRemove }) => {
-  return (
-    <div className="todoItem" onClick={() => {onToggle(id)}}>
-      <div className="remove" onClick={(e) => {
-          e.stopPropagation(); // onToggle 이 실행되지 않도록 함
-          onRemove(id)}
-        }>&times;</div>
+class TodoItem extends Component {
+  render() {
+    const { id, text, checked, onToggle, onRemove } = this.props;
 
-      <div className={`todo-text ${checked? 'checked' : ''}`}>{text}</div>
+    return (
+      <div className="todoItem" onClick={() => {onToggle(id)}}>
+        <div className="remove" onClick={(e) => {
+            e.stopPropagation(); // onToggle 이 실행되지 않도록 함
+            onRemove(id)}
+          }>&times;</div>
 
-      {
-        checked && <div className="check-mark">&#x2713;</div>
-      }
-    </div>
-  )
+          <div className={`todo-text ${checked? 'checked' : ''}`}>{text}</div>
+
+          {
+            checked && <div className="check-mark">&#x2713;</div>
+        }
+      </div>
+    )
+  }
 }
 
 export default TodoItem;
